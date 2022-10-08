09dc3286238e1b976dfe45ec0eb0a3a92feebd66
IvoElferink <61405522+IvoElferink@users.noreply.github.com>
2022-10-08 13:24:52 +0200

    Fixes and renaming

--- ivel.rb
+++ ivel.rb
@@ -1,5 +1,5 @@
-class HomebrewIvel < Formula
-  desc "Homebrew Tap by ivel"
+class ivel < Formula
+  desc "HomeBrew Tap by ivel test"
   homepage "https://github.com/IvoElferink/homebrew-ivel"
   url "https://github.com/IvoElferink/homebrew-ivel/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "1105e942b16fb03c38b6dc1bbd5802f53eefa3b9ea048a28b1296393eb848279"
