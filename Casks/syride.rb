417281eae017ae767a058a405b0f1ef5a67cb28a
IvoElferink <61405522+IvoElferink@users.noreply.github.com>
2022-10-08 16:48:49 +0200

    Added cask syride sys pc tool

---
+++ Casks/syride.rb
@@ -0,0 +1,20 @@
+cask "syride" do
+  version "6.31"
+  sha256 "1d3b9dc8a156aba4e973afbc591f7258e1544d5ae0851d2f044a0b9b4963b0ff"
+
+  url "https://www.syride.com/downloads/SYS_PC_Tool_setup_mac-#{version}.pkg"
+  name "Syride SYS PC Tool Software"
+  desc "Syride SYS PC Tool Software - Necessary for downloading flights, customizing screens, upgrading instrument..."
+  homepage "https://www.syride.com"
+
+  auto_updates true
+  depends_on macos: ">= :monterey"
+
+  app "SYS-PC-Tool.app"
+
+  zap trash: [
+    "~/Library/Saved Application State/Syride.SYS-PC-Tool.savedState",
+  ]
+end
+
+
