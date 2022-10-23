bae2ae40e6c6b94dbe67ab671f97339b4a772297
IvoElferink <61405522+IvoElferink@users.noreply.github.com>
2022-10-23 12:27:08 +0200

    Added multiviewer-for-f1-beta arch arm and intel support

--- Casks/multiviewer-for-f1-beta.rb
+++ Casks/multiviewer-for-f1-beta.rb
@@ -1,8 +1,16 @@
 cask "multiviewer-for-f1-beta" do
-  version "1.9.5,81907529"
-  sha256 "2f0101dc2b512596556ece518944591ee2bb9279d358c073e225e1120e37b3fb"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://releases.f1mv.com/download/#{version.csv.second}/MultiViewer.for.F1-#{version.csv.first}-x64.dmg"
+  on_intel do
+    version "1.9.5,81907529"
+    sha256 "2f0101dc2b512596556ece518944591ee2bb9279d358c073e225e1120e37b3fb"
+  end
+  on_arm do
+    version "1.9.5,81907283"
+    sha256 "b7252619f2fe82f8dabf2722c7681612310234cedb363faa5c76651776db3eba"
+  end
+
+  url "https://releases.f1mv.com/download/#{version.csv.second}/MultiViewer.for.F1-#{version.csv.first}-#{arch}.dmg"
   name "MultiViewer for F1"
   desc "Experience F1TV® like never before"
   homepage "https://beta.f1mv.com/"
@@ -10,7 +18,6 @@ cask "multiviewer-for-f1-beta" do
   auto_updates true
   conflicts_with cask: "multiviewer-for-f1"
   depends_on macos: ">= :monterey"
-  depends_on arch: :intel
 
   app "MultiViewer for F1.app"
 
