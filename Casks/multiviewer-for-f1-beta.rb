f5d424224f7733b6b5067fdccfc530e4037efd4b
IvoElferink <61405522+IvoElferink@users.noreply.github.com>
2022-11-11 16:36:14 +0100

    Fixed versions

--- Casks/multiviewer-for-f1-beta.rb
+++ Casks/multiviewer-for-f1-beta.rb
@@ -2,11 +2,11 @@ cask "multiviewer-for-f1-beta" do
   arch arm: "arm64", intel: "x64"
 
   on_intel do
-    version "1.10.2,84251119"
+    version "1.10.3,84271897"
     sha256 "adcaf5f8ccf1d997364ffaf310f0ef4909fce3172d8d84ad2b0494ad6454be92"
   end
   on_arm do
-    version "1.10.2,84250748"
+    version "1.10.3,84271516"
     sha256 "bac20bf8aebd1dfe7683f598b64af75eee8f7f3fcdc1ca763612f214ffea71d7"
   end
 
