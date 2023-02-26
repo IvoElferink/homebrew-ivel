6f14b6d24d59eb456ea61546a8ce8e328b787517
IvoElferink <61405522+IvoElferink@users.noreply.github.com>
2023-02-26 13:39:09 +0100

    Updated multiviewer-for-f1-beta.rb for arch arm with version 1.12.8,97051156

--- Casks/multiviewer-for-f1-beta.rb
+++ Casks/multiviewer-for-f1-beta.rb
@@ -6,8 +6,8 @@ cask "multiviewer-for-f1-beta" do
     sha256 "01d3a83583ac5ccd80fbdad45a7d7ad8cf80aa376812280e3353e3fc6e73856e"
   end
   on_arm do
-    version "1.12.6,96760701"
-    sha256 "8d2f2c937ce8c13eb093aa7235e531935ae89edccdc8f6a3a310209ee75f0de0"
+    version "1.12.8,97051156"
+    sha256 "4dd5f9b22f6397c0fc449ceeaa9cca716860856918a7a5e93ef09c008f127593"
   end
 
   url "https://releases.f1mv.com/download/#{version.csv.second}/MultiViewer.for.F1-#{version.csv.first}-#{arch}.dmg"
